c616b8fb8577c3824a4b057cfb09cbce80138b5c
rubilmax <rmilon@gmail.com>
2021-01-14 20:17:21 +0100

    Initial commit :tada:

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Home />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Home from './pages/Home/Home.vue';
+
+@Component({
+  components: {
+    Home,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
