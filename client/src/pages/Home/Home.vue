f9189d6b2d96bcd6a73d17f91a0574ac6db835e7
rubilmax <rmilon@gmail.com>
2021-01-14 22:05:04 +0100

    Refactored UI

--- client/src/pages/Home/Home.vue
+++ client/src/pages/Home/Home.vue
@@ -22,60 +22,9 @@
           </v-btn>
         </v-toolbar>
         <div class="container">
-          <v-card class="perf-card" elevation="1">
-            <v-card-title>Over the last 24 hours</v-card-title>
-            <v-divider />
-            <div class="perf-stats">
-              <div class="perf-stat">
-                <span>Min</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-              <div class="perf-stat">
-                <span>Avg</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-              <div class="perf-stat">
-                <span>Max</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-            </div>
-          </v-card>
-          <v-card class="perf-card" elevation="1">
-            <v-card-title>Over the last 7 days</v-card-title>
-            <v-divider />
-            <div class="perf-stats">
-              <div class="perf-stat">
-                <span>Min</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-              <div class="perf-stat">
-                <span>Avg</span>
-                <span class="accent--text">289ms</span>
-              </div>
-              <div class="perf-stat">
-                <span>Max</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-            </div>
-          </v-card>
-          <v-card class="perf-card" elevation="1">
-            <v-card-title>Over the last 28 days</v-card-title>
-            <v-divider />
-            <div class="perf-stats">
-              <div class="perf-stat">
-                <span>Min</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-              <div class="perf-stat">
-                <span>Avg</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-              <div class="perf-stat">
-                <span>Max</span>
-                <span class="accent--text">0.3s</span>
-              </div>
-            </div>
-          </v-card>
+          <PerfStatsCard title="Over the last 24 hours" />
+          <PerfStatsCard title="Over the last 7 days" />
+          <PerfStatsCard title="Over the last 28 days" />
         </div>
       </v-card>
     </v-dialog>
@@ -85,8 +34,13 @@
 <script lang="ts">
 import Vue from 'vue';
 
+import PerfStatsCard from './components/PerfStatsCard/PerfStatsCard.vue';
+
 export default Vue.extend({
   name: 'Home',
+  components: {
+    PerfStatsCard,
+  },
   data: () => ({
     isDialogOpen: false,
   }),
---
+++ client/src/pages/Home/components/PerfStatsCard/PerfStatsCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card class="perf-card" elevation="1">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-divider />
+    <div class="perf-stats">
+      <div class="perf-stat">
+        <span>Min</span>
+        <span class="accent--text">0.3s</span>
+      </div>
+      <div class="perf-stat">
+        <span>Avg</span>
+        <span class="accent--text">0.3s</span>
+      </div>
+      <div class="perf-stat">
+        <span>Max</span>
+        <span class="accent--text">0.3s</span>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'PerfStatsCard',
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.perf-card {
+  margin: 15px 20px;
+  padding: 0 25px;
+}
+
+.perf-stats {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px 0;
+}
+
+.perf-stat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 600;
+}
+</style>
